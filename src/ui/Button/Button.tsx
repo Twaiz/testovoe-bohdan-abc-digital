ffbc4dd8e8b38ea1fc21c9df0772ae72d640d4de
Twaiz <homenkobodya@gmail.com>
2024-09-28 19:25:23 +0300

    add[ButtonStyle]: added button styles for component Button

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -3,21 +3,31 @@ import { ReactNode } from "react";
 import styles from "./Button.module.css";
 
 type ButtonTypes = "button" | "submit" | "reset";
+type ButtonStyle = "basic" | "submit";
 
 interface IButtonProps {
   text: string | ReactNode;
   type?: ButtonTypes;
+  buttonStyle?: ButtonStyle;
   onClick?: () => void;
   disabled?: boolean;
 }
 
 const Button = (props: IButtonProps) => {
-  const { text, type = "button", onClick, disabled = false } = props;
+  const {
+    text,
+    type = "button",
+    buttonStyle = "basic",
+    onClick,
+    disabled = false,
+  } = props;
 
   return (
     <button
       type={type}
-      className={styles.button}
+      className={`${styles.button} ${
+        buttonStyle === "submit" && styles["button--submit"]
+      }`}
       disabled={disabled}
       onClick={onClick}
     >

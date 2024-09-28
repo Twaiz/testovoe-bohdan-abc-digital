4cbaa4423d68f2e2d9e17b9bbc7395409bc847a2
Twaiz <homenkobodya@gmail.com>
2024-09-28 11:20:52 +0300

    add[settings aliases]

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@root": path.resolve(__dirname, "./"),
+    },
+  },
+  css: {
+    modules: {
+      generateScopedName: "[name]__[local]_[hash:base64:2]",
+    },
+  },
+});

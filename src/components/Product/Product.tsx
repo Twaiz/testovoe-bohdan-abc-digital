ffbf0e356af49a1d0fa6fbffc95bade0c226e327
Twaiz <homenkobodya@gmail.com>
2024-09-28 16:01:14 +0300

    add[dynamic product data]: added dynamic data in products

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -2,26 +2,35 @@ import { FaEllipsisV } from "react-icons/fa";
 
 import styles from "./Product.module.css";
 
-const Product = () => {
+interface IProduct {
+  id: number;
+  productName: string;
+  description: string;
+  price: string;
+  currency: string;
+  quantity: string;
+  image: string;
+}
+
+const Product = ({ product }: { product: IProduct }) => {
+  const { productName, description, price, currency, quantity, image } =
+    product;
+
   return (
     <li className={styles.product}>
-      <img
-        src="https://img.jabko.ua/image/cache/catalog/products/2022/06/062308/macbook-air-starlight-gallery1-2-1397x1397.jpg.webp"
-        alt="oppo a58"
-        className={styles.product__image}
-      />
+      <img src={image} alt={productName} className={styles.product__image} />
 
       <div className={styles.wrapper}>
         <div className={styles.productInfo}>
-          <h2 className={styles.productInfo__name}>Oppo A58</h2>
-          <p className={styles.productInfo__description}>
-            8-core processor, 8GB RAM, 128GB
-          </p>
+          <h2 className={styles.productInfo__name}>{productName}</h2>
+          <p className={styles.productInfo__description}>{description}</p>
         </div>
 
         <div className={styles.productDetails}>
-          <span className={styles.productDetails__price}>7.800 ГРН</span>
-          <span className={styles.productDetails__quantity}>1</span>
+          <span className={styles.productDetails__price}>
+            {price} {currency}
+          </span>
+          <span className={styles.productDetails__quantity}>{quantity}</span>
         </div>
 
         <button type="button" className={styles.product__button}>
--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,15 +1,33 @@
+import { useEffect } from "react";
+import { observer } from "mobx-react-lite";
+
+import { productsStore } from "@/stores";
 import styles from "./Products.module.css";
 
 import Product from "@/components/Product";
 
-const Products = () => {
+interface IProduct {
+  id: number;
+  productName: string;
+  description: string;
+  price: string;
+  currency: string;
+  quantity: string;
+  image: string;
+}
+
+const Products = observer(() => {
+  useEffect(() => {
+    productsStore.getProducts();
+  }, []);
+
   return (
     <ul className={styles.products}>
-      {Array.from({ length: 10 }, (_, index) => (
-        <Product key={index} />
+      {productsStore.products.map((product: IProduct) => (
+        <Product key={product.id} product={product} />
       ))}
     </ul>
   );
-};
+});
 
 export default Products;
